b824edd7c1f445c237ff8c1ecba6c794a4518de5
dungw3b <dungweb@gmail.com>
2019-08-18 12:54:57 +0000

    update

--- example/main.go
+++ example/main.go
@@ -2,18 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"github.com/dungw3b/dwlog"
 )
 
 func main() {
-	app := &dwlog.Server {
-		Subject: "test",
-		Servers: []string{"nats://127.0.0.1:4222"},
-		Timeout: 10 * time.Second,
-	}
-
-	if err := app.Run(); err != nil {
-		fmt.Println(err)
-	}
+	
 }
--- server.go
+++ server.go
@@ -1,71 +1,25 @@
 package dwlog
 
 import (
-	"fmt"
-	"time"
-	"sync"
-	"strings"
-	"github.com/nats-io/nats.go"
+	"net"
 )
 
 type Server struct {
-	Subject string
-	Servers []string
-	Timeout time.Duration
-	conn *nats.Conn
-	encode *nats.EncodedConn
+
+}
+
+func (s *Server) GetMessage(ctx context.Context, m *Message) (*Response, error) {
+	return &Response{result: true}
 }
 
 func (s *Server) Run() error {
-	if err := s.Connect(); err != nil {
-		return err
-	}
-	defer s.Close()
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	_, err := s.encode.Subscribe(s.Subject, func(m *Message){
-		fmt.Println(m)
-		wg.Done()
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	wg.Wait()
-	return nil
-}
-
-func (s *Server) Connect() error {
-	if len(s.Subject) == 0 {
-		return fmt.Errorf("subject is required")
-	}
-	if len(s.Servers) == 0 {
-		return fmt.Errorf("list servers is required")
-	}
-	if s.Timeout == 0 {
-		s.Timeout = 20 * time.Second
-	}
-
-	var err error
-	s.conn, err = nats.Connect(strings.Join(s.Servers, ","), nats.Name("DWLog server"), nats.Timeout(s.Timeout), nats.NoEcho())
+	listen, err := net.Listen("tcp", 2701)
 	if err != nil {
 		return err
 	}
-	s.encode, err = nats.NewEncodedConn(s.conn, nats.JSON_ENCODER)
-	if err != nil {
-		s.Close()
-		return fmt.Errorf("encode json error")
-	}
-	return nil
-}
 
-func (s *Server) Close() {
-	if s.conn != nil {
-		if s.encode != nil {
-			s.encode.Close()
-		}
-		s.conn.Close()
-	}
+	grpcServer := grpc.NewServer()
+	RegisterLogServiceServer(grpcServer, &Server{})
+	grpcServer.Serve(listen)
+	return nil
 }
